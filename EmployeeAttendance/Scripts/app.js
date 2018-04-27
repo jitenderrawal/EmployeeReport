e7bfec4f576ab5f8c71327cc1fa26c6b7f840a40
Jitender Rawal <jitender.rawal@martindale.com>
2018-04-27 00:21:56 -0400

    Added some formatting, filter for date and UI cleanup

--- EmployeeAttendance/Scripts/app.js
+++ EmployeeAttendance/Scripts/app.js
@@ -12,6 +12,15 @@ app.controller("empCtrl", function ($http, $scope) {
 
 });
 
+app.filter("DateParser", function () {
+    var re = /\/Date\(([0-9]*)\)\//;
+    return function (x) {
+        var m = x.match(re);
+        if (m) return new Date(parseInt(m[1]));
+        else return null;
+    };
+});
+
 
 
 
