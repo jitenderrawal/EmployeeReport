294b839a2c85bdb996cb5d580866bb696e165de9
Jitender Rawal <jitender.rawal@martindale.com>
2018-04-26 23:24:48 -0400

    Add project files.

---
+++ EmployeeAttendance/Models/Repositories/EmployeeRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EmployeeAttendance.Models.Interfaces;
+
+namespace EmployeeAttendance.Models.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private EmployeesEntities _empContext;
+
+        public EmployeeRepository(EmployeesEntities context)
+        {
+            this._empContext = context;
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            return this._empContext.Employees.ToList<Employee>();
+        }
+    }
+}
---
+++ EmployeeAttendance/Scripts/app.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module("app", []);
+
+app.controller("empCtrl", function ($http, $scope) {
+
+    $scope.getEmployees = function (criteria) {
+        $http.get("/Employee").then(function (data) { $scope.employees = data.data }, function (error) { });
+    }
+
+    $scope.showDetails = function (employee) {
+        $scope.currentEmployee = employee;
+    }
+
+});
+
+
+
+
+
+
